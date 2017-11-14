8008f8bca41bdeac477b74af8097ab15d8820051
BR1312 <33597549+BR1312@users.noreply.github.com>
2017-11-14 22:26:14 +0800

    回传数据
    王宇娇是大傻子 如果回传成功 则证明以上结论

--- app/src/main/java/com/example/br1312/mypro01/login.java
+++ app/src/main/java/com/example/br1312/mypro01/login.java
@@ -1,10 +1,11 @@
+
 package com.example.br1312.mypro01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login extends AppCompatActivity {
-//明阳是天才 白蕊是天才 王宇娇 是个大智障 傻子 250
+//明阳是天才 白蕊是天才 王宇娇 是个大智障 傻子 250   //回传数据 王宇娇是大傻子
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
