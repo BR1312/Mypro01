5a89dadfc1b46b3450e843ed13f195827c5c1935
BR1312 <33597549+BR1312@users.noreply.github.com>
2017-11-14 22:27:09 +0800

    测试王宇娇是不是傻 如果成功 则上传成功

--- app/src/main/java/com/example/br1312/mypro01/login.java
+++ app/src/main/java/com/example/br1312/mypro01/login.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login extends AppCompatActivity {
-//明阳是天才 白蕊是天才 王宇娇 是个大智障 傻子 250   //回传数据 王宇娇是大傻子//ddd
+//明阳是天才 白蕊是天才 王宇娇 是个大智障 傻子 250   //回传数据 王宇娇是大傻子//dddddd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
